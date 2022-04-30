4ca35e4c0bada022464d9254b14bd691a1caa62b
Fani Masturina <71430949+fanimasturina@users.noreply.github.com>
2022-04-30 12:18:42 +0700

    Delete static/js directory

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-function col(){
-    // coldisplay = document.querySelector('input[name=gejalaChoosen:checked').value;
-    // document.querySelector("#scl").textContent="The selected radio content" + coldisplay;
-    alert('clicked');
-}
-
-function pil(){
-    
-}
