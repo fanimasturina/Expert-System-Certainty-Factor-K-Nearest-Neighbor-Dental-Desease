a9513597c69c654a712d5d9e993e0deae5146449
Fani Masturina <71430949+fanimasturina@users.noreply.github.com>
2022-04-30 13:02:04 +0700

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+function col(){
+    // coldisplay = document.querySelector('input[name=gejalaChoosen:checked').value;
+    // document.querySelector("#scl").textContent="The selected radio content" + coldisplay;
+    alert('clicked');
+}
+
+function pil(){
+    
+}
